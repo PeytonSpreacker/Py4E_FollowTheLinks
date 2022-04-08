8cefd9345c403a766164a28027a0a2130752a175
PeytonSpreacker <peyton@spreacker.com>
2022-04-08 09:53:07 -0500

    final commit

--- followthelinks.py
+++ followthelinks.py
@@ -8,10 +8,19 @@ ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
 url = input('Enter - ')
-html = urllib.request.urlopen(url, context=ctx).read()
-soup = BeautifulSoup(html, 'html.parser')
+count = int(input('Enter count: '))
+position = int(input('Enter position: '))
 
 # Retrieve all of the anchor tags
-tags = soup('a')
-for tag in tags:
-    print(tag.get('href', None))
+# tags = soup('a')
+# for tag in tags:
+    # print(tag.get('href', None))
+
+for i in range (0, count):
+    html = urllib.request.urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    tags = soup('a')
+    link = (tags[position-1].get('href'))
+    url = link
+print('Retrieving:',url)
+print('Name:',url[39:45])
